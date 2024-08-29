d5032b339cd863402d78a71adb6913c2bbd343a1
gianluca <dillpixel1@gmail.com>
2024-08-29 16:52:41 -0500

    fix: added code comments.

--- src/welcoming/model/developer.entity.js
+++ src/welcoming/model/developer.entity.js
@@ -1,12 +1,27 @@
+/**
+ * Developer entity
+ * @description
+ * This class represents a developer entity
+ * It has a first name and a last name.
+ */
 export class Developer {
     firstName;
     lastName;
 
+    /**
+     * Constructor
+     * @param firstName
+     * @param lastName
+     */
     constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    /**
+     * Get the full name of the developer
+     * @returns {string|string}
+     */
     get fullName() {
         return this.firstName = null || this.lastName == null ? "Unknown" : `${this.firstName} ${this.lastName}`.trim();
     }
