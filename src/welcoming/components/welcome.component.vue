2971200a1eb0dbc121aa007967e49eab14db8a0f
gianluca <dillpixel1@gmail.com>
2024-08-29 16:29:05 -0500

    feat(welcoming): added welcome component.

---
+++ src/welcoming/components/welcome.component.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "welcome",
+  props: {
+    firstName: String,
+    lastName: String
+  },
+  methods: {
+    developerName(){
+      let developer = new Developer(this.firstName, this.lastName);
+      return developer .fullName;
+    }
+  }
+}
+</script>
+
+<template>
+  <p>Hello {{ developerName }}. Now You are a Vue Developer.</p>
+</template>
+
+<style scoped>
+
+</style>
